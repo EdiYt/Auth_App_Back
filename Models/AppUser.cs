8a9024bd3bb1a488a27127f436e40ff2b9c0c356
Erick <erickvillaseor@yahoo.com.mx>
2025-08-02 23:02:55 -0600

    Se modifico el modelo de AppUser y se hizo la migracion

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -4,5 +4,6 @@ using Microsoft.AspNetCore.Identity;
     public class AppUser : IdentityUser
     {
         public string? FullName {  get; set; }
-
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
     }
--- obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3ad3bd70237d73ef2259338f4a2a1d3a5a7229")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b703ed55f3b3a40efae263b051a626096722cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
